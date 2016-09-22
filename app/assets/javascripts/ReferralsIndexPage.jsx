b8e562897cf6734429c1fa9b36e87a579abf7432
Nate Jones and Nora Lin <pair+suman@casecommons.org>
2016-09-22 14:21:28 -0400

    Add ResponseTime filter to report index page
    [#128068239]

--- app/assets/javascripts/ReferralsIndexPage.jsx
+++ app/assets/javascripts/ReferralsIndexPage.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import ReferralsFilter from 'ReferralsFilter'
 import ReferralsTable from 'ReferralsTable'
 import {request} from 'utils/http'
 
@@ -8,20 +9,43 @@ export default class ReferralsIndexPage extends React.Component {
     this.state = {
       referrals: [],
     }
+    this.updateIndex = this.updateIndex.bind(this)
   }
 
   componentDidMount() {
-    const xhr = request('GET', '/referrals.json', null, null)
+    this.updateIndex()
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if(prevProps.location != this.props.location) {
+      this.updateIndex()
+    }
+  }
+
+  updateIndex() {
+    const {pathname, search} = this.props.location
+    const url = `${pathname}.json${search}`
+    const xhr = request('GET', url, null, null)
     xhr.done((xhrResp) => {
       this.setState({referrals: xhrResp.responseJSON})
     })
   }
 
   render() {
+    const {query} = this.props.location
     return (
       <div>
-        <ReferralsTable referrals={this.state.referrals}/>
+        <div className='col-md-3'>
+          <ReferralsFilter query={query} />
+        </div>
+        <div className='col-md-9'>
+          <ReferralsTable referrals={this.state.referrals} />
+        </div>
       </div>
     )
   }
+}
+
+ReferralsIndexPage.propTypes = {
+  location: React.PropTypes.object,
 }
--- spec/javascripts/ReferralsIndexPageSpec.jsx
+++ spec/javascripts/ReferralsIndexPageSpec.jsx
@@ -1,13 +1,20 @@
 import $ from 'jquery'
 import React from 'react'
 import ReactDOM from 'react-dom'
+import ReferralsFilter from 'ReferralsFilter'
 import ReferralsIndexPage from 'ReferralsIndexPage'
 import ReferralsTable from 'ReferralsTable'
 import TestUtils from 'react-addons-test-utils'
 
 describe('ReferralsIndexPage', () => {
+  it('renders ReferralsFilter', () => {
+    const view = TestUtils.renderIntoDocument(<ReferralsIndexPage location={{}} />)
+    const filters = TestUtils.scryRenderedComponentsWithType(view, ReferralsFilter)
+    expect(filters.length).toEqual(1)
+  })
+
   it('renders ReferralsTable', () => {
-    const view = TestUtils.renderIntoDocument(<ReferralsIndexPage />)
+    const view = TestUtils.renderIntoDocument(<ReferralsIndexPage location={{}} />)
     const tables = TestUtils.scryRenderedComponentsWithType(view, ReferralsTable)
     expect(tables.length).toEqual(1)
   })
